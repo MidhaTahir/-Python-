88c74b33047f08347ba316e90482d8707be5f8a3
Midha <midhatahirkhan2011@gmail.com>
2019-08-12 08:16:18 -0700

    Lists as Stacks

---
+++ stacks.py
@@ -0,0 +1,12 @@
+# Stack is LIFO - last in First out
+#Stack has two functions 1) Push 2) Pop 
+#List methods append and pop can easily be used for this purpose 
+stack = [4,5,6]
+stack.append(7) #it appends to the right side of the list 
+stack.append(8)
+print(stack)
+stack.pop() #it pops the last element
+stack.pop()
+print(stack)
+
+
